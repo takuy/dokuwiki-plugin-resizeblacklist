e60ef30a5e849336f42f808c5ca241e956946126
Sam <1631095+takuy@users.noreply.github.com>
2020-09-27 13:38:03 -0400

    remnants of copypasta

--- action.php
+++ action.php
@@ -2,27 +2,25 @@
  
 if(!defined('DOKU_INC')) die();
 
-if(class_exists("helper_plugin_redirect", TRUE)) {
 
-    class action_plugin_resizeblacklist extends DokuWiki_Action_Plugin { 
-    
-        /**
-         * Register its handlers with the DokuWiki's event controller
-         */
-        public function register(Doku_Event_Handler $controller) {
-            $controller->register_hook('MEDIA_RESIZE', 'BEFORE', $this, 'can_resize');
-        }
-        /**
-         * @param Doku_Event $event  event object by reference
-         * @param mixed      $param  empty
-         * @param string     $advise the advise the hook receives
-         */
-        function can_resize(&$event, $param) {
-            $EXT = $event->data["ext"];
-            $MIME = $event->data["ext"];
-            $isImage = substr($MIME, 0, 5) == 'image';
+class action_plugin_resizeblacklist extends DokuWiki_Action_Plugin { 
 
-            return !( $isImage && (in_array("all", $this->getConf("no_resize")) || in_array($EXT, $this->getConf["no_resize"])) );
-        }
+    /**
+     * Register its handlers with the DokuWiki's event controller
+     */
+    public function register(Doku_Event_Handler $controller) {
+        $controller->register_hook('MEDIA_RESIZE', 'BEFORE', $this, 'can_resize');
+    }
+    /**
+     * @param Doku_Event $event  event object by reference
+     * @param mixed      $param  empty
+     * @param string     $advise the advise the hook receives
+     */
+    function can_resize(&$event, $param) {
+        $EXT = $event->data["ext"];
+        $MIME = $event->data["ext"];
+        $isImage = substr($MIME, 0, 5) == 'image';
+        
+        return !( $isImage && (in_array("all", $this->getConf("no_resize")) || in_array($EXT, $this->getConf["no_resize"])) );
     }
 }
